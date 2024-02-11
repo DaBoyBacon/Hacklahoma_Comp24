4f361e05c04ea7d8f1fe6d1eff66b3e1dafda5fa
TubAleeLo <tub-a-lo@outlook.com>
2024-02-11 10:16:03 -0600

    update

--- Charlies_Cosmic_Conquest/Assets/Levels/Scripts/EnemyDamage.cs
+++ Charlies_Cosmic_Conquest/Assets/Levels/Scripts/EnemyDamage.cs
@@ -6,11 +6,23 @@ public class EnemyDamage : MonoBehaviour
 {
     private int damage = 1;
     public PlayerHealth playerHealth;
-    
+    public PlayerScript playerMovement;
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            playerMovement.knockBackCounter = playerMovement.knockBackTotalTime;
+            if(collision.transform.position.x <= transform.position.x)
+            {
+                playerMovement.knockFromRight = true;
+            }
+            if (collision.transform.position.x > transform.position.x)
+            {
+                playerMovement.knockFromRight = false;
+            }
+
             playerHealth.TakeDamage(damage);
         }
     }
